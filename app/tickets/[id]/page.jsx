41fa139c59ed7783403c12c799bffce3c7d563b7
Barklegne Eshetu <barklegne.eshetu@gmail.com>
2024-06-21 10:38:13 +0300

    mock server delay removed

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -13,7 +13,7 @@ export const generateStaticParams = async () => {
 
 const getTicket = async (id) => {
 	// imitate a slow network request
-	await new Promise((resolve) => setTimeout(resolve, 3000))
+	// await new Promise((resolve) => setTimeout(resolve, 3000))
 
 	const response = await fetch(`http://localhost:4000/tickets/${id}`)
 
